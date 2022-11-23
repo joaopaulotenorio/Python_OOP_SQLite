3515497d1720547149cd80c8e64dd639c205efd4
João Paulo Tenório <44954773+joaopaulotenorio@users.noreply.github.com>
2022-11-23 23:07:59 +0000

    Iniciando Orientação a Objetos

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-# 
+#Importa nosso arquivo Pessoa.py no diretório model
+from model.Pessoa import Pessoa
+
+#Exemplo de uso
+poyatos = Pessoa(1, "Henrique Poyatos")
+print(poyatos)
+
---
+++ model/Pessoa.py
@@ -0,0 +1,10 @@
+class Pessoa:
+  #Atributos
+  id = None
+  nome = None
+
+  #Método Construtor
+  def _init_(self, id, nome):
+    self.id = id
+    self.nome = nome
+
